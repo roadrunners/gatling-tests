a92d7d8bbafe9789a052e3ed08108f62e164adf5
Karan Misra <karan@erodov.com>
2013-08-30 05:27:19 +0530

    can configure the min/max pause times

--- simulations/urlshortener/Options.scala
+++ simulations/urlshortener/Options.scala
@@ -11,13 +11,13 @@ object Options {
   val createWait = sys.env("GATLING_CREATE_WAIT").toInt seconds
   val createUsers = sys.env("GATLING_CREATE_USERS").toInt users
   val createRampTime = sys.env("GATLING_CREATE_RAMPTIME").toInt seconds
-  val createPauseMin = 100 milliseconds
-  val createPauseMax = 300 milliseconds
+  val createPauseMin = sys.env("GATLING_CREATE_PAUSEMIN").toInt milliseconds
+  val createPauseMax = sys.env("GATLING_CREATE_PAUSEMAX").toInt milliseconds
 
   val retrieveDuration = sys.env("GATLING_RETRIEVE_DURATION").toInt minutes
   val retrieveWait = sys.env("GATLING_RETRIEVE_WAIT").toInt seconds
   val retrieveUsers = sys.env("GATLING_RETRIEVE_USERS").toInt users
   val retrieveRampTime = sys.env("GATLING_RETRIEVE_RAMPTIME").toInt seconds
-  val retrievePauseMin = 100 milliseconds
-  val retrievePauseMax = 300 milliseconds
+  val retrievePauseMin = sys.env("GATLING_RETRIEVE_PAUSEMIN").toInt milliseconds
+  val retrievePauseMax = sys.env("GATLING_RETRIEVE_PAUSEMAX").toInt milliseconds
 }
