90c7f16715f0911dbcdc14f499a8b1c5450b78be
Karan Misra <karan@erodov.com>
2013-09-13 01:15:17 +0530

    increase load

--- simulations/urlshortener/Options.scala
+++ simulations/urlshortener/Options.scala
@@ -7,17 +7,17 @@ import io.gatling.http.Predef._
 object Options {
   val endpoint = sys.env("GATLING_ENDPOINT")
 
-  val createDuration = 1 minutes
+  val createDuration = 5 minutes
   val createWait = 0 seconds
-  val createUsers = 100 users
-  val createRampTime = 30 seconds
+  val createUsers = 200 users
+  val createRampTime = 10 seconds
   val createPauseMin = 100 milliseconds
   val createPauseMax = 300 milliseconds
 
-  val retrieveDuration = 1 minutes
-  val retrieveWait = 60 seconds
-  val retrieveUsers = 100 users
-  val retrieveRampTime = 30 seconds
+  val retrieveDuration = 4 minutes
+  val retrieveWait = 50 seconds
+  val retrieveUsers = 300 users
+  val retrieveRampTime = 10 seconds
   val retrievePauseMin = 100 milliseconds
   val retrievePauseMax = 300 milliseconds
 }
