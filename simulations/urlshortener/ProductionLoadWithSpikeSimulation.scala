058ce056c2c58179994277377292e89cec0e6024
Karan Misra <karan@erodov.com>
2013-09-17 02:51:33 +0530

    Revert "fmj"
    This reverts commit 667839ab2d94fcdecea2d639fb5d83aae29f956f.

--- simulations/urlshortener/ProductionLoadWithSpikeSimulation.scala
+++ simulations/urlshortener/ProductionLoadWithSpikeSimulation.scala
@@ -9,8 +9,8 @@ import scala.concurrent.duration._
 class ProductionLoadWithSpikeSimulation extends Simulation {
   val httpProtocol = http.baseURL(Options.endpoint).disableFollowRedirect
 
-  setUp(CreateScenario.scn(duration = 110 seconds).inject(nothingFor(0 seconds), ramp(500 users) over (10 seconds)),
-    RetrieveScenario.scn(duration = 110 seconds).inject(nothingFor(0 seconds), ramp(2000 users) over (10 seconds)),
-    CreateScenario.scn(name = "Spike Creation of URLs", duration = 30 seconds).inject(nothingFor(30 seconds), ramp(1000 users) over (10 seconds)))
+  setUp(CreateScenario.scn(duration = 110 seconds).inject(nothingFor(0 seconds), ramp(100 users) over (10 seconds)),
+    RetrieveScenario.scn(duration = 110 seconds).inject(nothingFor(0 seconds), ramp(600 users) over (10 seconds)),
+    CreateScenario.scn(name = "Spike Creation of URLs", duration = 30 seconds).inject(nothingFor(30 seconds), ramp(400 users) over (10 seconds)))
     .protocols(httpProtocol)
 }
