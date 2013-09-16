8f793b94a98a605d7d3d41cfd32e656406a03847
Karan Misra <karan@erodov.com>
2013-09-16 08:21:03 +0530

    shorter, aggressive

--- simulations/urlshortener/ProductionLoadWithSpikeSimulation.scala
+++ simulations/urlshortener/ProductionLoadWithSpikeSimulation.scala
@@ -9,8 +9,8 @@ import scala.concurrent.duration._
 class ProductionLoadWithSpikeSimulation extends Simulation {
   val httpProtocol = http.baseURL(Options.endpoint).disableFollowRedirect
 
-  setUp(CreateScenario.scn(duration = 350 seconds).inject(nothingFor(0 seconds), ramp(100 users) over (10 seconds)),
-    RetrieveScenario.scn(duration = 300 seconds).inject(nothingFor(0 seconds), ramp(600 users) over (60 seconds)),
-    CreateScenario.scn(name = "Spike Creation of URLs", duration = 60 seconds).inject(nothingFor(120 seconds), ramp(400 users) over (20 seconds)))
+  setUp(CreateScenario.scn(duration = 110 seconds).inject(nothingFor(0 seconds), ramp(100 users) over (10 seconds)),
+    RetrieveScenario.scn(duration = 110 seconds).inject(nothingFor(0 seconds), ramp(600 users) over (10 seconds)),
+    CreateScenario.scn(name = "Spike Creation of URLs", duration = 30 seconds).inject(nothingFor(30 seconds), ramp(400 users) over (10 seconds)))
     .protocols(httpProtocol)
 }
